77efa432e4f4c8334b16af05aba2cc8ed6351afe
gbarone42 <badph3@gmail.com>
2025-04-16 00:03:38 +0200

    ok:

--- scripts/sync_matches.js
+++ scripts/sync_matches.js
@@ -5,11 +5,11 @@ const { ethers } = require("hardhat");
 
 async function getMatchesFromDB() {
   const client = new Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
   });
 
   await client.connect();
